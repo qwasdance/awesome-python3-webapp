8b7fbb61eaef1adf71c9c3d70f4a8f91fd0c9f0a
dxlu <dxlu@winnermedical.com>
2018-12-26 10:03:30 +0800

    add css

--- www/handlers.py
+++ www/handlers.py
@@ -1,18 +1,25 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-'url handlers'
+__author__ = 'Michael Liao'
 
-import re,time,json,logging,hashlib,base64,asyncio
+' url handlers '
 
-from coroweb import get,post
+import re, time, json, logging, hashlib, base64, asyncio
 
-from models import User,Comment,Blog,next_id
+from coroweb import get, post
 
-@get
-async def index(request):
-    users=await User.findAll()
+from models import User, Comment, Blog, next_id
+
+@get('/')
+def index(request):
+    summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    blogs = [
+        Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
+        Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600),
+        Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time()-7200)
+    ]
     return {
-        '__template__':'test.html',
-        'users':users
+        '__template__': 'blogs.html',
+        'blogs': blogs
     }
